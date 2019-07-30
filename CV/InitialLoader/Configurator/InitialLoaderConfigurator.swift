362a9ce5d42fdfca4646b7c1d53c908cf03dc8c7
Ivan Erwin Lopez Ansaldo <ivan.lopez@globant.com>
2019-07-30 10:57:00 -0500

    Create Configurators

---
+++ CV/InitialLoader/Configurator/InitialLoaderConfigurator.swift
@@ -0,0 +1,9 @@
+//
+//  InitialLoaderConfigurator.swift
+//  CV
+//
+//  Created by Ivan Erwin Lopez Ansaldo on 7/30/19.
+//  Copyright © 2019 Ivan Erwin Lopez Ansaldo. All rights reserved.
+//
+
+import Foundation
